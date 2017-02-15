e5db2d9d30f7742715525998059ede5b2c6be767
jackmtr <jackmtr@hotmail.com>
2017-02-15 10:05:23 -0800

    Initial commiting of empty project

---
+++ src/com/contography/employeeList/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Project : com.contography.employeeList 
+ * File : Main.java
+ * Date : Feb 15, 2017
+ * Time : 10:01:17 AM
+ */
+package com.contography.employeeList;
+
+/**
+ * @author Jackie
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
